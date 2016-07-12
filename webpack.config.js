fe58f2ce1c193286673728bbccf9424d0ffa2de9
Megan Smith <megan@tokaypress.com>
2016-07-12 11:33:13 -0700

    (init) set up client files

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var path = require('path');
+
+var config = {
+  devtool: 'inline-source-map',
+  context: path.join(__dirname, 'src'),
+  entry: [
+    './main.js',
+  ],
+  output: {
+    path: path.join(__dirname, 'www'),
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel'],
+      },
+      {
+        test: /\.scss$/,
+        loaders: ["style", "css", "sass"]
+      },
+      {
+        test: /\.css$/, loader: "style-loader!css-loader"
+      },
+    ],
+  },
+  resolveLoader: {
+    root: [
+      path.join(__dirname, 'node_modules'),
+    ],
+  },
+  resolve: {
+    root: [
+      path.join(__dirname, 'node_modules'),
+    ],
+  },
+};
+module.exports = config;
