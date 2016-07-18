5148e52274ae4544aa9a7adb1942a14e5848a0a3
Megan Smith <megan@tokaypress.com>
2016-07-18 14:11:35 -0700

    Converted all ids to serial type

--- server/db/addRecipes.js
+++ server/db/addRecipes.js
@@ -7,8 +7,8 @@ recipes.forEach((recipe) => {
   .then((data) => {
     var res = [];
     if (data) {
-      recipe.ingredients.forEach((ingredient) => {
-        db.oneOrNone('INSERT into $1~($2~, $3~, $4~, $5~) VALUES(default, $6, $7, $8) ON CONFLICT DO NOTHING', ['ingredients_recipes', 'id', 'ingredient_num', 'ingredient_text', 'recipes_id', ind2, ingredient, data.id])
+      recipe.ingredients.forEach((ingredient, ind) => {
+        db.oneOrNone('INSERT into $1~($2~, $3~, $4~, $5~) VALUES(default, $6, $7, $8) ON CONFLICT DO NOTHING', ['ingredients_recipes', 'id', 'ingredient_num', 'ingredient_text', 'recipes_id', ind, ingredient, data.id])
         .then(() => {
           console.log('Success in ingredients_recipes');
         })
@@ -20,11 +20,9 @@ recipes.forEach((recipe) => {
     }
   })
   .then((id) => {
-    var res2 = [];
     if (id) {
-      recipe.steps.forEach((step, ind3) => {
-        var uniqueInd3 = (ind3 + 3) * id;
-        db.oneOrNone('INSERT into $1~($2~, $3~, $4~, $5~) VALUES($6, $7, $8, $9) ON CONFLICT DO NOTHING', ['steps_recipes', 'id', 'step_num', 'step_text', 'recipes_id', uniqueInd3, ind3, step, id])
+      recipe.steps.forEach((step, ind) => {
+        db.oneOrNone('INSERT into $1~($2~, $3~, $4~, $5~) VALUES(default, $6, $7, $8) ON CONFLICT DO NOTHING', ['steps_recipes', 'id', 'step_num', 'step_text', 'recipes_id', ind, step, id])
         .then(() => {
           console.log('Success in steps_recipes');
         })
@@ -38,13 +36,10 @@ recipes.forEach((recipe) => {
   .then((id) => {
     var res3 = [];
     if (id) {
-      recipe.categories.forEach((cat, ind4) => {
-        var uniqueInd4 = (ind4 + 4) * id;
-        console.log('CAT', cat);
-        console.log('UNIQUE ID', uniqueInd4);
+      recipe.categories.forEach((cat) => {
         db.oneOrNone('INSERT into $1~($2~, $3~) VALUES(default, $4) ON CONFLICT DO NOTHING', ['categories', 'id', 'name', cat])
-        .then((id) => {
-          console.log('Success in categories', id);
+        .then(() => {
+          console.log('Success in categories');
         })
         .catch((error) => {
           console.error('Error in categories', error);
@@ -56,12 +51,10 @@ recipes.forEach((recipe) => {
   .then((id) => {
     if (id) {
       var res4 = [];
-      recipe.categories.forEach((cat, ind4) => {
-        var uniqueInd4 = (ind4 + 4) * id;
-        var uniqueInd5 = (uniqueInd4 + 1) * id;
+      recipe.categories.forEach((cat) => {
         db.one('SELECT id from categories where name=$1', [cat])
         .then((data) => {
-          db.oneOrNone('INSERT into $1~($2~, $3~, $4~) VALUES($5, $6, $7) ON CONFLICT DO NOTHING', ['categories_recipes', 'id', 'categories_id', 'recipes_id', uniqueInd5, data.id, id])
+          db.oneOrNone('INSERT into $1~($2~, $3~, $4~) VALUES(default, $5, $6) ON CONFLICT DO NOTHING', ['categories_recipes', 'id', 'categories_id', 'recipes_id', data.id, id])
           .then(() => {
             console.log('Success in categories_recipes');
           })
